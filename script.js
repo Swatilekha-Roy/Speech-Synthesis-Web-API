5aa13dfc44079d54f8732b44f1e04fa688eac517
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-05-31 12:22:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,11 +30,26 @@ const wait = ms => {
 };
 
 const playSong = async () => {
-    await sing(`Oh, to see without my eyes`, 0.9);
+    await sing(`Oh, to see without my eyes-`, 0.9);
     await sing(`The first time that you kissed me!`, 0.7);
     await wait(1000);
     await sing(`Boundless by the time I cried-`, 0.9);
-    await sing('I built your walls around me!', 0.7);
+    await sing(`I built your walls around me!`, 0.7);
+    await wait(1000);
+    await sing(`White noise, what an awful sound-`, 0.9);
+    await sing(`Fumbling by rogue river`, 0.7);
+    await wait(1000);
+    await sing(`Feel my feet above the ground-`, 0.9);
+    await sing(`Hand of God deliver me`, 0.7);
+    await wait(900);
+    await sing(`Ohhhhh oh oh oh oh is meeeee`, 0.8);
+    await(`The first time that you touched me`, 0.7);
+    await wait(900);
+    await sing(`Ohhhhh will wonders ever cease`, 0.8);
+    await sing(`Blessed be the mystery`,0.8);
+    await sing(`Of love`,0.7);
+    await wait(500);
+    await sing(`Honestly, do you have nothing better to do?`);
 };
 
 document.querySelector("#play").addEventListener("click", () => {
