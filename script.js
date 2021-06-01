1ecce2f5a978e2756811267c97e69364a9017331
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-06-01 10:50:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,19 +29,19 @@ const wait = ms => {
 };
 
 const playSong = async () => {
-    await sing(`Ohhh, to see without my eyes-`, 0.9);
+    await sing(`Ohhh, to see without my eyes`, 0.9);
     await sing(`The first time that youuuu kiiiissed me!`, 0.7);
     await wait(1000);
-    await sing(`Boundless by the time I cried-`, 0.9);
+    await sing(`Boundless by the time I cried`, 0.9);
     await sing(`I built your walls aaaround me!`, 0.7);
     await wait(1000);
-    await sing(`White noise, what an awful sound-`, 0.9);
+    await sing(`White noise, what an awful sound`, 0.9);
     await sing(`Fumbling by rogue river`, 0.7);
     await wait(1000);
-    await sing(`Feel my feet above the ground-`, 0.9);
+    await sing(`Feel my feet above the ground`, 0.9);
     await sing(`Hand of God delllliver me`, 0.7);
     await wait(900);
-    await sing(`Ohhhhh oh oh oh oh is meeeee-`, 0.8);
+    await sing(`Ohhhhh oh oh oh oh is meeeee`, 0.8);
     await sing(`The first time that youuuu touched me`, 0.7);
     //await wait(900);
     await sing(`Ohhhhh will wonders ever cease`, 0.8);
